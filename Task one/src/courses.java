21521aeed3d6deb7607718a4aa9399ef79f6c8ef
kaufahk <kaufahk@gmail.com>
2026-09-22 13:17:03 -0700

    commit 2
    In the NV, from line five to line nine, a constructor (courses) was created and initialized the object variables

--- Task one/src/courses.java
+++ Task one/src/courses.java
@@ -2,4 +2,9 @@ public class courses {
     String coursename;
     int numberOfStudents;
     static int totalStudents;
+    public courses(String c,int n,int t){
+        coursename=c;
+        numberOfStudents=n;
+        totalStudents=t;
+    }
 }
