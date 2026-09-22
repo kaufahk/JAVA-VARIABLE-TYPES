b557a76048166f2030dc845140fed97988d23493
kaufahk <kaufahk@gmail.com>
2026-09-22 13:11:17 -0700

    commit 1
    In the NV, from line six to line eight, local variables were created to calculate the average number of students on a java file (Main)

--- Task one/src/Main.java
+++ Task one/src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         courses obj1= new courses("COA",5,9);
         courses obj2= new courses("Comp Applications",3,7);
         courses obj3= new courses("DSA",2,6);
+        int totalNumberOfStudents= obj1.numberOfStudents+obj2.numberOfStudents+obj3.numberOfStudents;
+        int NumberOfCourses= 3;
+        int AverageStudents= totalNumberOfStudents/NumberOfCourses;
     }
 }
